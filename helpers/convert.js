cfb6052b784278f90bcbc92d01d3518627456ecf
asyncasad <asyncasad@gmail.com>
2023-08-22 11:18:18 +0530

    add code to create markdown table for github

--- helpers/convert.js
+++ helpers/convert.js
@@ -1,18 +1,7 @@
-// const fs = require("fs");
-
-// const iconsDir = fs.readdirSync("./public/icons");
-// const icons = {};
-// for (const icon of iconsDir) {
-//   const name = icon.replace(".svg", "").toLowerCase();
-//   icons[name] = String(fs.readFileSync(`./public/icons/${icon}`));
-// }
-
-// if (!fs.existsSync("./src/dist")) fs.mkdirSync("./src/dist");
-// fs.writeFileSync("./src/dist/icons.json", JSON.stringify(icons));
-
 const fs = require("fs");
-
+const tableHead = `| Icon ID | Icon |\n| ------- | ---- |\n`;
 const categoryDirs = fs.readdirSync("./public/icons");
+let tableRows = ``;
 const icons = {};
 for (const categoryDir of categoryDirs) {
   const themeDirs = fs.readdirSync(`./public/icons/${categoryDir}`);
@@ -24,11 +13,22 @@ for (const categoryDir of categoryDirs) {
       const name = `${categoryDir}-${themeDir}-${icon
         .replace(".svg", "")
         .toLocaleLowerCase()}`;
+      tableRows =
+        tableRows +
+        `| \`${name}\` | <img src="./public/icons/${categoryDir}/${themeDir}/${icon}" width="48"> |\n`;
       icons[name] = String(
         fs.readFileSync(`./public/icons/${categoryDir}/${themeDir}/${icon}`)
       );
     }
   }
 }
+const markdownTable = tableHead + tableRows;
+fs.writeFile("./src/icon_table.md", markdownTable, (err) => {
+  if (err) {
+    console.error("Error writing file:", err);
+  } else {
+    console.log("Markdown table generated successfully!");
+  }
+});
 if (!fs.existsSync("./src/dist")) fs.mkdirSync("./src/dist");
 fs.writeFileSync("./src/dist/icons.json", JSON.stringify(icons));
